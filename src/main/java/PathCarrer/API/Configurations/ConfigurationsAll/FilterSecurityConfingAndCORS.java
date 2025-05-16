7c0dbbb422d1b26572736946bf28a80bb1ec3ac6
Alison Martins <alisonmarteso@gmail.com>
2025-05-16 00:12:39 -0300

    Refernciamenteo do CORS

--- src/main/java/PathCarrer/API/Configurations/ConfigurationsAll/FilterSecurityConfingAndCORS.java
+++ src/main/java/PathCarrer/API/Configurations/ConfigurationsAll/FilterSecurityConfingAndCORS.java
@@ -56,7 +56,7 @@ public class FilterSecurityConfingAndCORS {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(CorsClient));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
